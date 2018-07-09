c9f8f8f6313e947f5a27db428923ffced2b301d7
Dave Newton <davelnewton@gmail.com>
2018-07-09 09:19:23 -0400

    master Example for 51225050

---
+++ ruby/51225050/README.md
@@ -0,0 +1,2 @@
+https://stackoverflow.com/questions/51225050/ruby-on-rails-show-external-json-code-in-view
+
---
+++ ruby/51225050/standalone_example.rb
@@ -0,0 +1,55 @@
+require 'json'
+
+s = <<EOF
+{
+    "result": {
+        "2332860": {
+            "id": 2332860,
+            "name": "NAME 1",
+            "capping": {
+                "quantity": 1,
+                "frequency": 1440
+            },
+            "status": 0,
+            "price": 159,
+            "publisher_ad_types": []
+        },
+        "2335970": {
+            "id": 2335970,
+            "name": "NAME 2",
+            "capping": {
+                "quantity": 1,
+                "frequency": 1440
+            },
+            "status": 0,
+            "price": 159,
+            "publisher_ad_types": []
+        },
+        "2343470": {
+            "id": 2343470,
+            "name": "NAME 3",
+            "capping": {
+                "quantity": 1,
+                "frequency": 1440
+            },
+            "status": 0,
+            "price": 170,
+            "publisher_ad_types": []
+        }
+    },
+    "request_metadata": {
+        "limit": 50,
+        "offset": 0,
+        "count": 11
+    }
+}
+EOF
+
+obj    = JSON.parse(s)
+result = obj['result']
+
+result.each do |id, val|
+  id, name = val['id'], val['name']
+  puts "ID:   #{id}"
+  puts "Name: #{name}"
+end
